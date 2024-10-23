ed9f2cf37136c9a5ccb4766bddb14445bb8017c9
BermuGod28 <thejuanjo1128@gmail.com>
2024-10-22 22:19:56 -0500

    Cambios Gerente

--- src/main/java/com/example/demo/repository/GerenteRepository.java
+++ src/main/java/com/example/demo/repository/GerenteRepository.java
@@ -1,11 +1,26 @@
 package com.example.demo.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.model.Cargo;
 import com.example.demo.model.Gerente;
-
+import com.example.demo.model.Regional;
+import java.util.List;
 @Repository
 public interface GerenteRepository extends JpaRepository<Gerente, Long> {
+    
+     // Encuentra un gerente por su email
     Gerente findByEmail(String email);
+
+    // Encuentra gerentes por nombre
+    List<Gerente> findByNombre(String nombre);
+
+    // Encuentra gerentes por cargo
+    List<Gerente> findByCargo(Cargo cargo);
+
+    // Encuentra gerentes por regional
+    List<Gerente> findByRegional(Regional regional);
+
 }
